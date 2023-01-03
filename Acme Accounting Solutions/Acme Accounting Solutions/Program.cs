749926a071bfa1ff3150ac19c74bbf0d193cd7bd
Zelena Palijo <zpali853@gmail.com>
2023-01-02 16:11:53 -0800

    Added Acme Accounting Systems assignment code

---
+++ Acme Accounting Solutions/Acme Accounting Solutions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Acme_Accounting_Solutions
+{
+    class Program
+    {
+        static void Main()
+        {
+            string str1 = "Welcome to Acme Accounting Systems \n" +
+                "Remember, we're \"accounting\" on you!";
+
+            Console.WriteLine(str1);
+            Console.ReadLine();
+        }
+    }
+}
