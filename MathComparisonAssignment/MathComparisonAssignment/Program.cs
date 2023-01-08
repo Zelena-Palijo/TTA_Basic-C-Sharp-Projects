ead4aebf1062ea8221688e2997a7af658d718959
Zelena Palijo <zpali853@gmail.com>
2023-01-07 18:41:16 -0800

    Created math comparison assignent project

---
+++ MathComparisonAssignment/MathComparisonAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathComparisonAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            //string str1 = "Anonymous Income Comparison Program";
+            //Console.WriteLine(str1);
+
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.ReadLine();
+        }
+    }
+}
