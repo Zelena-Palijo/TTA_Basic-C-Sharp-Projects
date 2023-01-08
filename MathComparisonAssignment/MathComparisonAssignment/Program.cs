1a187bbee77350fabaefa44a4ea2bc25dfe9808a
Zelena Palijo <zpali853@gmail.com>
2023-01-07 18:59:12 -0800

    added hourly rate

--- MathComparisonAssignment/MathComparisonAssignment/Program.cs
+++ MathComparisonAssignment/MathComparisonAssignment/Program.cs
@@ -14,7 +14,19 @@ namespace MathComparisonAssignment
             //Console.WriteLine(str1);
 
             Console.WriteLine("Anonymous Income Comparison Program");
-            Console.ReadLine();
+            //Console.ReadLine();
+            //Program must start with this statement
+
+            Console.WriteLine("Person 1");
+
+            string str1 = "What is your hourly rate?";
+            Console.WriteLine(str1);
+            string hourlyRate = Console.ReadLine();
+            int hourlyRate = Convert.ToInt32(hourlyRate);
+
+
+
+
         }
     }
 }
