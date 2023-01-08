d677e9677ce715e6fcc5e098697be1d2719b60d0
Zelena Palijo <zpali853@gmail.com>
2023-01-07 19:20:50 -0800

    added weekly rate of both people, posit comparison

--- MathComparisonAssignment/MathComparisonAssignment/Program.cs
+++ MathComparisonAssignment/MathComparisonAssignment/Program.cs
@@ -20,20 +20,38 @@ namespace MathComparisonAssignment
             Console.WriteLine("Person 1");
 
             Console.WriteLine("What is your hourly rate?");
-            string hourlyRate = Console.ReadLine();
-            int hRate = Convert.ToInt32(hourlyRate);
+            string hourlyRate1 = Console.ReadLine();
+            int hRate1 = Convert.ToInt32(hourlyRate1);
 
-            //Re-use previous str1 for format
+            //Re-use previous for format
             Console.WriteLine("How many hours do you work per week?");
-            string weeklyHour = Console.ReadLine();
-            int wHour = Convert.ToInt32(weeklyHour);
+            string weeklyHour1 = Console.ReadLine();
+            int wHour1 = Convert.ToInt32(weeklyHour1);
             //convert to int to do math
 
-            int product = hRate * wHour;
-            Console.WriteLine(product);
+            Console.WriteLine("Person 2");
+
+            Console.WriteLine("What is your hourly rate?");
+            string hourlyRate2 = Console.ReadLine();
+            int hRate2 = Convert.ToInt32(hourlyRate2);
+
+            //Re-use previous for format
+            Console.WriteLine("How many hours do you work per week?");
+            string weeklyHour2 = Console.ReadLine();
+            int wHour2 = Convert.ToInt32(weeklyHour2);
+            //convert to int to do math
+
+            int product = hRate1 * wHour1;
+            Console.WriteLine("Annual salary of Person 1: " + product);
             Console.ReadLine();
             //multiple to get weekly rate
 
+            int product2 = hRate2 * wHour2;
+            Console.WriteLine("Annual salary of Person 2: " + product2);
+            Console.ReadLine();
+            //multiple to get weekly rate
+
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
 
         }
     }
