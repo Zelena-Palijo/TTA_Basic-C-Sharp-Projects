5d3f921f817063b7072f2448536bcd23f5ef8f6f
Zelena Palijo <zpali853@gmail.com>
2023-01-09 16:48:00 -0800

    Created car insurance app assignment

---
+++ CarInsuranceAssignment/CarInsuranceAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Application questions
+
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int ageNum = Convert.ToInt32(age);
+            //ask for age, convert to int for value comparison
+
+            Console.WriteLine("True or False: Have you ever had a DUI?");
+            Console.ReadLine();
+            // needs to be t/f for bool later, so only give t/f as option
+
+
+        }
+    }
+}
