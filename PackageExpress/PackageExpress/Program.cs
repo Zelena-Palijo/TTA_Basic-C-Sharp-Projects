a9541d9b07b65a95c75ecea347407012271e2681
Zelena Palijo <zpali853@gmail.com>
2023-01-11 21:41:45 -0800

    added weight limit

--- PackageExpress/PackageExpress/Program.cs
+++ PackageExpress/PackageExpress/Program.cs
@@ -11,7 +11,17 @@ namespace PackageExpress
         static void Main()
         {
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            //Console.ReadLine();
+
+            Console.WriteLine("Please enter the package weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            string result = weight >= 50 ? "Package too big to be shiped via Package Express." : " ";
+
+            Console.WriteLine(result);
             Console.ReadLine();
+
+           
         }
     }
 }
