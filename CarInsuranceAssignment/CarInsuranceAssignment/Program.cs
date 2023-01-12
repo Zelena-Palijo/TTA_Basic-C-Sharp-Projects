618a7031fb9267a14ddca926fbd067f9ea53a26c
Zelena Palijo <zpali853@gmail.com>
2023-01-11 20:02:27 -0800

    cleaned up code

--- CarInsuranceAssignment/CarInsuranceAssignment/Program.cs
+++ CarInsuranceAssignment/CarInsuranceAssignment/Program.cs
@@ -13,19 +13,16 @@ namespace CarInsuranceAssignment
             //Application questions
 
             Console.WriteLine("What is your age?");
-            string age = Console.ReadLine();
-            int ageNum = Convert.ToInt32(age);
+            int ageNum = Convert.ToInt32(Console.ReadLine());
             //ask for age, convert to int for value comparison
 
             string str1 = "True or False: Have you ever had a DUI?";
             Console.WriteLine(str1);
-            string safety = Console.ReadLine();
-            bool safe = Convert.ToBoolean(safety);
+            bool safe = Convert.ToBoolean(Console.ReadLine());
             // convert to boolean to use later
 
             Console.WriteLine("How many speeding tickets do you have?");
-            string ticket = Console.ReadLine();
-            int ticketNum = Convert.ToInt32(ticket);
+            int ticketNum = Convert.ToInt32(Console.ReadLine());
             // convert to int for value comparison
 
             //Check qualifications -big if/else statement here?
