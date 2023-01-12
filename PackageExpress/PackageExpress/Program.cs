44ac4ad844c9eb25ea69720d824f8440bb0c73f4
Zelena Palijo <zpali853@gmail.com>
2023-01-11 21:24:06 -0800

    created package express console app

---
+++ PackageExpress/PackageExpress/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageExpress
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.ReadLine();
+        }
+    }
+}
