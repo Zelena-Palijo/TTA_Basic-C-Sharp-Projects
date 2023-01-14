a5f3dd4fc935ff93c75dca90398be26acdd29bcb
Zelena Palijo <zpali853@gmail.com>
2023-01-13 18:24:24 -0800

    added switch to do while loop

--- LoopsSwitch/LoopsSwitch/Program.cs
+++ LoopsSwitch/LoopsSwitch/Program.cs
@@ -10,30 +10,48 @@ namespace LoopsSwitch
     {
         static void Main()
         {
-            //int i = 0;
-            //while (i<10) 
-            //{
-            //    Console.WriteLine("Times this has happened: "+ i); 
-            //    Console.ReadLine(); // if not here, then end program too fast?
-            //    i++; // add 1 each time
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine("Times this has happened: " + i);
+                Console.ReadLine(); // if not here, then end program too fast?
+                i++; // add 1 each time
 
-            //}
-            //Console.WriteLine("Thank you!");
-            //Console.ReadLine();
+            }
+            Console.WriteLine("Thank you!");
+            Console.ReadLine();
 
             Console.WriteLine("How old are you?");
             int age = Convert.ToInt32(Console.ReadLine()); // convert age to integer
             bool howOld = age == 29; //true only if equals to 29
 
-            do
+            do //had to do switch because wouldn't work without it? 
             {
-                Console.WriteLine("You are not 29."); // respond if not 29 otherwise end
-                Console.ReadLine();
+                switch (age)
+                {
+                    case 28:
+                        Console.WriteLine("Oh you're young");
+                        Console.WriteLine("How old are you?");
+                        age = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 30:
+                        Console.WriteLine("Oh you're old");
+                        Console.WriteLine("How old are you?");
+                        age = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("Oh wow");
+                        Console.WriteLine("How old are you?");
+                        age = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29: //true statement
+                        Console.WriteLine("Just like me!");
+                        howOld = true;
+                        break;
+                }
             }
-            while (!howOld);
-
+            while (!howOld); // if howOld = false or not equal to 29
             Console.Read();
-            System.Environment.Exit(0);
         }
     }
 }
