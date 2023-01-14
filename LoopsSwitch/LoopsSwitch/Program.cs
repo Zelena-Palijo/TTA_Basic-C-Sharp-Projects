310a0324d73d152cb4a649cc9d9d7bfffceaa1cb
Zelena Palijo <zpali853@gmail.com>
2023-01-13 17:19:25 -0800

    Created file, added do/while loop

---
+++ LoopsSwitch/LoopsSwitch/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsSwitch
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            bool howOld = age == 29;
+
+            do
+            {
+                Console.WriteLine("Cool!");
+                Console.ReadLine();
+            }
+            while (age > 29);
+            Console.Read();
+        }
+    }
+}
