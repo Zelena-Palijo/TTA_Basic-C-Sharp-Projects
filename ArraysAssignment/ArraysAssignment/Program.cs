58fd656f7acb05ac73e8b3752e851a5a37304b0e
Zelena Palijo <zpali853@gmail.com>
2023-01-15 12:21:12 -0800

    switched from while loop to simple if/else

--- ArraysAssignment/ArraysAssignment/Program.cs
+++ ArraysAssignment/ArraysAssignment/Program.cs
@@ -5,23 +5,23 @@ class Program
     {
         static void Main()
         {
-        string[] shoppingList = new string[] { "eggs", "pasta", "bacon", "pepper", "parm" };
-        Console.WriteLine("Please select a number between 0 to 4 to see an item from the list.");
-        int choice = Convert.ToInt32(Console.ReadLine());
+            string[] shoppingList = new string[] { "eggs", "pasta", "bacon", "pepper", "parm" };
+            Console.WriteLine("Please select a number between 0 to 4 to see an item from the list.");
+            int choice = Convert.ToInt32(Console.ReadLine());
         
-        if(choice <= 4)
-        {
-            Console.WriteLine(shoppingList[choice]);
-            Console.ReadLine();
-        }
-        else
-        {
-            Console.WriteLine("Sorry, your request cannot be completed. \n " +
-                "Please choose a number between 0 to 4.");
-            Console.ReadLine();
-        }
+            if (choice <= 4)
+            {
+                Console.WriteLine(shoppingList[choice]);
+                Console.ReadLine();
+           
+            }
+            else
+            {
+               Console.WriteLine("Sorry, your request cannot be completed. \n " +
+               "Please choose a number between 0 to 4.");
+               Console.ReadLine();
+            }
 
-        
         }
     }
 
