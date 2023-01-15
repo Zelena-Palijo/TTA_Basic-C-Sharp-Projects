bb4dabfd93972969ada6ff8a18dfd15069afaffd
Zelena Palijo <zpali853@gmail.com>
2023-01-15 11:51:01 -0800

    added list array

--- ArraysAssignment/ArraysAssignment/Program.cs
+++ ArraysAssignment/ArraysAssignment/Program.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 
 class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+        string[] shoppingList = new string[] { "eggs", "pasta", "bacon", "pepper", "parm" };
+        Console.WriteLine("Please select a number from 1 to 5 to see an item from the list.");
+
+        
         }
     }
 
