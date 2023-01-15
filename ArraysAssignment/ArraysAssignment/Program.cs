38918af533d00ae3cac10f8486609b60b31058d8
Zelena Palijo <zpali853@gmail.com>
2023-01-15 12:12:41 -0800

    adds ask and display from shopping list

--- ArraysAssignment/ArraysAssignment/Program.cs
+++ ArraysAssignment/ArraysAssignment/Program.cs
@@ -7,6 +7,9 @@ class Program
         {
         string[] shoppingList = new string[] { "eggs", "pasta", "bacon", "pepper", "parm" };
         Console.WriteLine("Please select a number from 1 to 5 to see an item from the list.");
+        int choice = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(shoppingList[choice]);
+        Console.ReadLine();
 
         
         }
