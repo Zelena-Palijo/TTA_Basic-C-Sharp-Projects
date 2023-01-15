fcfc418140159b4861e3bb20ef25d3ec061e9bdc
Zelena Palijo <zpali853@gmail.com>
2023-01-15 12:15:09 -0800

    if/else if index not included

--- ArraysAssignment/ArraysAssignment/Program.cs
+++ ArraysAssignment/ArraysAssignment/Program.cs
@@ -6,10 +6,20 @@ class Program
         static void Main()
         {
         string[] shoppingList = new string[] { "eggs", "pasta", "bacon", "pepper", "parm" };
-        Console.WriteLine("Please select a number from 1 to 5 to see an item from the list.");
+        Console.WriteLine("Please select a number between 0 to 4 to see an item from the list.");
         int choice = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine(shoppingList[choice]);
-        Console.ReadLine();
+        
+        if(choice <= 4)
+        {
+            Console.WriteLine(shoppingList[choice]);
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Sorry, your request cannot be completed. \n " +
+                "Please choose a number between 0 to 4.");
+            Console.ReadLine();
+        }
 
         
         }
