5c4ce7db26e8f2cf32b98d467bc2f683608935da
Zelena Palijo <zpali853@gmail.com>
2023-01-15 10:59:08 -0800

    convert 1 string to upper case

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -10,16 +10,21 @@ namespace Strings
     {
         static void Main()
         {
-            string str1 = "Hello, my name is";
-            Console.WriteLine(str1);
+            string str1 = "Hello, my name is ";
+            //Console.WriteLine(str1);
             
             string str2 = "Dr. ";
-            Console.WriteLine(str2);
+            //Console.WriteLine(str2);
 
             string str3 = "Seuss";
-            Console.WriteLine(str3);
+            //Console.WriteLine(str3);
 
+            Console.WriteLine(str1 + str2 + str3); //concatenate 3 strings
+            string sentence = Console.ReadLine();
 
+            string up1 = str1.ToUpper(); //make str1 all uppercase
+            Console.WriteLine(up1); 
+            Console.ReadLine();
         }
     }
 }
