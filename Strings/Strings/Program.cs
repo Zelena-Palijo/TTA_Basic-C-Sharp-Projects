a934116818d130ecaad03ecb16a5a1668ee39dcd
Zelena Palijo <zpali853@gmail.com>
2023-01-15 10:53:39 -0800

    write 3 strings

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string str1 = "Hello, my name is";
+            Console.WriteLine(str1);
+            
+            string str2 = "Dr. ";
+            Console.WriteLine(str2);
+
+            string str3 = "Seuss";
+            Console.WriteLine(str3);
+
+
+        }
+    }
+}
