011560e779d315f24f7006a8da1a487e5c4ef7a5
Zelena Palijo <zpali853@gmail.com>
2023-01-15 14:57:03 -0800

    setup, add string array

---
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> colors = new List<string> { "red", "green", "yellow", "orange" };
+        }
+    }
+}
