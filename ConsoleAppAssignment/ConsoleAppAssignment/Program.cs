a914b47aa1bd349c5af3531e3808a36f5d75d4ae
Zelena Palijo <zpali853@gmail.com>
2023-01-15 18:08:41 -0800

    adds user input to array/list

--- ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -7,7 +7,33 @@ namespace ConsoleAppAssignment
     {
         static void Main(string[] args)
         {
-            List<string> colors = new List<string> { "red", "green", "yellow", "orange" };
+            //string[] colorArray = { "brown", "black", "white", "cerulean" }; // array of strings
+            //List<string> colors = new List<string>(colorArray); //convert to list to add items
+            List<string> colors = new List<string>() { "brown", "black", "white", "cerulean" }; 
+            //made it a list, but can make it an array also
+
+            Console.WriteLine("What's your favourite color?"); // prompt to input text
+            string newColor = Console.ReadLine();
+            colors.Add(newColor); // adds user's text input to the end of the string
+
+            foreach (string color in colors) //loops through and adds the input to end of each string
+            {
+                Console.WriteLine(color + " " + newColor);
+            }
+
+            //second loop
+            Console.WriteLine("Here are some other people's favorite colors:");
+            foreach (string color in colors)
+            {
+                Console.WriteLine(color);
+            }
+            Console.ReadLine();
+            
+
+            
+            
+
+
         }
     }
 }
