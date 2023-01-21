e57e37acc7224425aace4d9333c53fc3eda8fde0
Zelena Palijo <zpali853@gmail.com>
2023-01-21 11:52:33 -0800

    create new class Card, add property

---
+++ TwentyOne/TwentyOne/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Card
+    {
+        //Giving Card class a property 
+        public string Suit { get; set; }
+        public string Face { get; set; }
+    }
+}
