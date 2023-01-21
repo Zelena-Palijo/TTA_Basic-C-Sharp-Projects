99a5d9624eca271f3292633ff59f0370dde17831
Zelena Palijo <zpali853@gmail.com>
2023-01-21 11:12:03 -0800

    adds division, displays quotients

--- ExceptionHandlingAssignment/ExceptionHandlingAssignment/Program.cs
+++ ExceptionHandlingAssignment/ExceptionHandlingAssignment/Program.cs
@@ -10,6 +10,16 @@ namespace ExceptionHandlingAssignment
     {
         static void Main(string[] args)
         {
+            List<int> numList = new List<int>() { 18, 36, 54, 72, 90 };
+            Console.WriteLine("Pick a number.");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            foreach (int n in numList)
+            {
+                int quotient = n / choice;
+                Console.WriteLine(n + " divided by " + choice + " equals " + quotient);
+            }
+            Console.ReadLine();
         }
     }
 }
