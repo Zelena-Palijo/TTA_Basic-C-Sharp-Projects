2eefdbda34257bf1623e59fbceac4938fed2dea6
Zelena Palijo <zpali853@gmail.com>
2023-01-21 12:15:57 -0800

    lists for foreach loop for deck creation

--- TwentyOne/TwentyOne/Deck.cs
+++ TwentyOne/TwentyOne/Deck.cs
@@ -11,6 +11,28 @@ namespace TwentyOne
         //add constructor here
         public Deck()
         {
+            //Cards = new List<Card>();
+            //Card cardOne = new Card();
+            //cardOne.Face = "Two";
+            //cardOne.Suit = "Hearts";
+            //Cards.Add(cardOne);
+
+            //easier way using nested for/each loop instead of writing it out
+            //list of all possible suits
+            List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
+            List<string> Faces = new List<string>()
+            {
+                "Two", "Three", "Four", "Five", "Six", "Seven",
+                "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"
+            };
+
+            foreach (string face in Faces)
+            {
+                foreach(string suit in Suits)
+                {
+
+                }
+            }
 
         }
 
