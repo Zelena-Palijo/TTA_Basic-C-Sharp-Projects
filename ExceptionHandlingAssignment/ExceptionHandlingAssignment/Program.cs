1c09d46dc0fb46a41b13185b1a336b9c5c0f267d
Zelena Palijo <zpali853@gmail.com>
2023-01-21 11:17:56 -0800

    added error messages

--- ExceptionHandlingAssignment/ExceptionHandlingAssignment/Program.cs
+++ ExceptionHandlingAssignment/ExceptionHandlingAssignment/Program.cs
@@ -10,16 +10,45 @@ namespace ExceptionHandlingAssignment
     {
         static void Main(string[] args)
         {
-            List<int> numList = new List<int>() { 18, 36, 54, 72, 90 };
-            Console.WriteLine("Pick a number.");
-            int choice = Convert.ToInt32(Console.ReadLine());
-
-            foreach (int n in numList)
+            try
             {
-                int quotient = n / choice;
-                Console.WriteLine(n + " divided by " + choice + " equals " + quotient);
+                List<int> numList = new List<int>() { 18, 36, 54, 72, 90 };
+                Console.WriteLine("Pick a number.");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int n in numList)
+                {
+                    int quotient = n / choice;
+                    Console.WriteLine(n + " divided by " + choice + " equals " + quotient);
+                }
+                Console.ReadLine();
             }
-            Console.ReadLine();
+            //if person chooses 0
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please choose a non-zero number.");
+                return;
+            }
+            // if person inputs string
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please choose a whole number.");
+                return;
+            }
+            //for general issues
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            finally
+            {
+                // message that they emerged from try/catch block
+                Console.WriteLine("The program will now end."); 
+                Console.ReadLine();
+            }
+
+            
         }
     }
 }
