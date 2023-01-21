bf3f67282a3f7fda8af34ac7d9a159f12ffa526e
Zelena Palijo <zpali853@gmail.com>
2023-01-21 13:53:26 -0800

    fixed method 1, added method 2

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class Math
+    {
+        //First method allows multiplication by 72
+        public static int MultiplyBy72(int num1)
+        {
+            int mult;
+            mult = num1 * 72;
+            return mult;
+
+        }
+
+        //second method
+        public static int DivideBy3(int num1)
+        {
+            int div;
+            div = num1 / 3;
+            return div;
+        }
+    }
+
+        
+}
