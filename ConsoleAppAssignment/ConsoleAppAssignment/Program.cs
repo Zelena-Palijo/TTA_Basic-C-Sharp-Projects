10efa2cd74bbbf3d5d4353e4edff70a0550df636
Zelena Palijo <zpali853@gmail.com>
2023-01-20 18:08:42 -0800

    commenting in

--- ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -9,143 +9,143 @@ namespace ConsoleAppAssignment
         {
             //// PART 1
 
-            ////string[] colorArray = { "brown", "black", "white", "cerulean" }; // array of strings
-            ////List<string> colors = new List<string>(colorArray); //convert to list to add items
-            //List<string> colors = new List<string>() { "brown", "black", "white", "cerulean" };
-            ////made it a list, but can make it an array also
+            //string[] colorArray = { "brown", "black", "white", "cerulean" }; // array of strings
+            //List<string> colors = new List<string>(colorArray); //convert to list to add items
+            List<string> colors = new List<string>() { "brown", "black", "white", "cerulean" };
+            //made it a list, but can make it an array also
 
-            //Console.WriteLine("What's your favourite color?"); // prompt to input text
-            //string newColor = Console.ReadLine();
-            //colors.Add(newColor); // adds user's text input to the end of the string
+            Console.WriteLine("What's your favourite color?"); // prompt to input text
+            string newColor = Console.ReadLine();
+            colors.Add(newColor); // adds user's text input to the end of the string
 
-            //foreach (string color in colors) //loops through and adds the input to end of each string
-            //{
-            //    Console.WriteLine(color + " " + newColor);
-            //}
+            foreach (string color in colors) //loops through and adds the input to end of each string
+            {
+                Console.WriteLine(color + " " + newColor);
+            }
 
-            ////second loop
-            //Console.WriteLine("\n Here are some other people's favorite colors: ");
-            //foreach (string color in colors)
-            //{
-            //    Console.WriteLine(color);
-            //}
-            //Console.ReadLine();
-
-
-            //// PART 2 
-            ////creating infinite loop
-
-            //int count = 0; //add condition to break infinite loop
-            //while (true)
-            //{
-            //    Console.WriteLine("Wake up!"); //infinitely creates loop
-
-            //    if (count++ >= 40) //limiting loop to 40, so once 40 times, then will stop
-            //    {
-            //        break;
-            //    }
-
-            //}
-            //Console.WriteLine("Good-bye.");
-            //Console.ReadLine();
+            //second loop
+            Console.WriteLine("\n Here are some other people's favorite colors: ");
+            foreach (string color in colors)
+            {
+                Console.WriteLine(color);
+            }
+            Console.ReadLine();
 
 
-            //// PART 3
-            //int numCount = 0;
-            //while (true) // infinite loop conditions from before
-            //{
-            //    Console.WriteLine("Keep it moving");
+            // PART 2 
+            //creating infinite loop
 
-            //    if (numCount++ < 10) // comparison is <
-            //    {
-            //        break; //stop loop at 10
-            //    }
+            int count = 0; //add condition to break infinite loop
+            while (true)
+            {
+                Console.WriteLine("Wake up!"); //infinitely creates loop
 
-            //}
-            //Console.WriteLine("OK, that's good.");
-            //Console.ReadLine();
+                if (count++ >= 40) //limiting loop to 40, so once 40 times, then will stop
+                {
+                    break;
+                }
 
-            //int number = 100;
-            //while (true) // infinite loop conditions from before
-            //{
-            //    Console.WriteLine("blah");
+            }
+            Console.WriteLine("Good-bye.");
+            Console.ReadLine();
 
-            //    if (number-- <= 20) // comparison is <=
-            //    {
-            //        break; //stop loop at 20
-            //    }
 
-            //}
-            //Console.WriteLine("OK.");
-            //Console.ReadLine();
+            // PART 3
+            int numCount = 0;
+            while (true) // infinite loop conditions from before
+            {
+                Console.WriteLine("Keep it moving");
+
+                if (numCount++ < 10) // comparison is <
+                {
+                    break; //stop loop at 10
+                }
+
+            }
+            Console.WriteLine("OK, that's good.");
+            Console.ReadLine();
+
+            int number = 100;
+            while (true) // infinite loop conditions from before
+            {
+                Console.WriteLine("blah");
+
+                if (number-- <= 20) // comparison is <=
+                {
+                    break; //stop loop at 20
+                }
+
+            }
+            Console.WriteLine("OK.");
+            Console.ReadLine();
 
 
             // PART 4
-            //List<string> coffees = new List<string>() { "latte", "americano", "iced", "regular" };
+            List<string> coffees = new List<string>() { "latte", "americano", "iced", "regular" };
 
-            //Console.WriteLine("Please choose one of the following: \n " +
-            //    "latte, americano, iced, regular");
-            //string order = Console.ReadLine();
+            Console.WriteLine("Please choose one of the following: \n " +
+                "latte, americano, iced, regular");
+            string order = Console.ReadLine();
 
-            //foreach (string coffee in coffees) // goes through each item in list 
+            foreach (string coffee in coffees) // goes through each item in list 
+            {
+
+                if (coffees.Contains(order)) // if the order on the list
+                {
+                    Console.WriteLine(coffees.IndexOf(order)); // write index of item
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, that item is not on the menu. Please choose a menu item.");
+                    Console.ReadLine();
+                }
+            }
+            // tried alternate with while loop, but could not get it to work see below:
+            //while (!coffees.Contains(order)) //made it if not contains, then will keep saying
+            //    //until gives option on list
             //{
-
-            //    if (coffees.Contains(order)) // if the order on the list
-            //    {
-            //        Console.WriteLine(coffees.IndexOf(order)); // write index of item
-            //        Console.ReadLine();
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Sorry, that item is not on the menu. Please choose a menu item.");
-            //        Console.ReadLine();
-            //    }
+            //    Console.WriteLine("Sorry, that item is not on the menu. Please choose a menu item.");
+            //    Console.ReadLine();
+            //    break;
             //}
-                // tried alternate with while loop, but could not get it to work see below:
-                //while (!coffees.Contains(order)) //made it if not contains, then will keep saying
-                //    //until gives option on list
-                //{
-                //    Console.WriteLine("Sorry, that item is not on the menu. Please choose a menu item.");
-                //    Console.ReadLine();
-                //    break;
-                //}
-                //Console.WriteLine(coffees.IndexOf(order)); // write index of item
-                //Console.ReadLine();
-            
+            //Console.WriteLine(coffees.IndexOf(order)); // write index of item
+            //Console.ReadLine();
+
 
             // PART 5
-            //List<string> chores = new List<string>() { "dishes", "laundry", "vacuum", "dishes", "windows" };
-            //Console.WriteLine("What chore will you start with today: dishes, laundry, vacuum, windows.");
-            //string task = Console.ReadLine();
+            List<string> chores = new List<string>() { "dishes", "laundry", "vacuum", "dishes", "windows" };
+            Console.WriteLine("What chore will you start with today: dishes, laundry, vacuum, windows.");
+            string task = Console.ReadLine();
 
-            //if (chores.Contains(task))
-            //{
-            //    foreach (string chore in chores)
-            //    {
-            //        Console.WriteLine(chores.IndexOf(task));
-            //        Console.ReadLine();
-            //    }
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Sorry, please choose a task on the list.");
-            //    Console.ReadLine();
+            if (chores.Contains(task)) //if the list contains the task then 
+            {
+                foreach (string chore in chores)
+                {
+                    Console.WriteLine(chores.IndexOf(task)); //write the index of the task that matches
+                    Console.ReadLine();
+                }
+            }
+            else // if choose something not on list
+            {
+                Console.WriteLine("Sorry, please choose a task on the list.");
+                Console.ReadLine();
 
-            //}
+            }
 
-            //foreach (string chore in chores)
-            //{
-            //    while (chores.Contains(task))
-            //    {
-            //        Console.WriteLine(chores.IndexOf(task));
-            //        Console.ReadLine();
+            foreach (string chore in chores)
+            {
+                while (chores.Contains(task))
+                {
+                    Console.WriteLine(chores.IndexOf(task));
+                    Console.ReadLine();
 
-            //    }
-            //    Console.WriteLine("Sorry, please choose a task on the list.");
-            //    Console.ReadLine();
+                }
+                Console.WriteLine("Sorry, please choose a task on the list.");
+                Console.ReadLine();
 
 
-            //}
+            }
 
 
             // PART 6
