35f307d4205076c20702e099861b90960bcfb4bd
Zelena Palijo <zpali853@gmail.com>
2023-01-22 13:25:08 -0800

    Added void method Deal()

--- TwentyOne/TwentyOne/Dealer.cs
+++ TwentyOne/TwentyOne/Dealer.cs
@@ -11,5 +11,14 @@ namespace TwentyOne
         public string Name { get; set; }
         public Deck Deck { get; set; }
         public int Balance { get; set; }
+
+        public void Deal(List<Card> Hand)
+        {
+            Hand.Add(Deck.Cards.First()); 
+            // First is a built-in method that takes the first of the list
+            Console.WriteLine(Deck.Cards.First().ToString() + "\n");
+            Deck.Cards.RemoveAt(0); 
+            //Add first item, print to console, then remove it from deck
+        }
     }
 }
