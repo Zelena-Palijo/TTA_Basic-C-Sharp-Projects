f8f41480b12d43727c55a5eaba559bcd117410d9
Zelena Palijo <zpali853@gmail.com>
2023-01-22 09:34:47 -0800

    added method with output string

--- ClassSubmissionAssignment/ClassSubmissionAssignment/Math.cs
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/Math.cs
@@ -8,10 +8,20 @@ namespace ClassSubmissionAssignment
 {
     class Math
     {
+        //Method 1 - Void, divide by 2
         public static void Half(int num1)
         {
             Console.WriteLine("Half of " + num1 + " equals " + (num1 / 2));
             Console.ReadLine();
+        }
+
+        //Method 2 - output parameters
+        public static int Add10(int num2, out string message)
+        {
+            int add;
+            add = num2 + 10;
+            message = "It's been done.";
+            return add;
         }
     }
 }
