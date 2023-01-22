5d2592eafce1f84ff373b0e8cdf7249514af1263
Zelena Palijo <zpali853@gmail.com>
2023-01-22 10:45:34 -0800

    added game properties, listplayer to console

--- TwentyOne/TwentyOne/Game.cs
+++ TwentyOne/TwentyOne/Game.cs
@@ -8,5 +8,16 @@ namespace TwentyOne
 {
     public class Game
     {
+        public List<string> Players { get; set; }
+        public string Name { get; set; }
+        public string Dealer { get; set; }
+
+        public void ListPlayers()
+        {
+            foreach (string player in Players)
+            {
+                Console.WriteLine(player);
+            }
+        }
     }
 }
