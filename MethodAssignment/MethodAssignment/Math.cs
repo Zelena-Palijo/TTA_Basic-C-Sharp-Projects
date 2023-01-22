dc6a776c847c36379125feec85bc20bedaca7db5
Zelena Palijo <zpali853@gmail.com>
2023-01-21 18:25:36 -0800

    created method with optional num2

---
+++ MethodAssignment/MethodAssignment/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    public class Math
+    {
+        //first method
+        public static int MultiplyBy5(int num1, int num2 = 2) //num2 is defined, and optional
+        {
+            int mult;
+            mult = num1 * 5 * num2; //default will multiple by 10
+            return mult;
+        }
+    }
+}
