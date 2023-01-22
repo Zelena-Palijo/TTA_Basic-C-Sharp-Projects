19b3adb7449a0cc01ee4cda0ba60134c8f65e913
Zelena Palijo <zpali853@gmail.com>
2023-01-21 16:14:37 -0800

    created new class, added method

---
+++ MainMethodAssignment/MainMethodAssignment/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class Math
+    {
+        //First method
+        public static int MultiplyBy5(int num1)
+        {
+            int mult;
+            mult = num1 * 5;
+            return mult;
+        }
+    }
+}
