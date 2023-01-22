4742cec32d986b958709ef0682525f6be802376c
Zelena Palijo <zpali853@gmail.com>
2023-01-22 09:15:46 -0800

    Call method Half

--- ClassSubmissionAssignment/ClassSubmissionAssignment/Program.cs
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/Program.cs
@@ -10,6 +10,13 @@ namespace ClassSubmissionAssignment
     {
         static void Main(string[] args)
         {
+            //Calling method to divide by 2
+
+            Console.WriteLine("Pick a number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Math.Half(num1);
+
+            
         }
     }
 }
