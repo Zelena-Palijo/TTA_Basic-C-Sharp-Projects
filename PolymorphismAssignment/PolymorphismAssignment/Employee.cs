48182b1994b818ba2602efdd585ebe0eb2b4f49d
Zelena Palijo <zpali853@gmail.com>
2023-01-22 15:35:53 -0800

    add quit to employee class

--- PolymorphismAssignment/PolymorphismAssignment/Employee.cs
+++ PolymorphismAssignment/PolymorphismAssignment/Employee.cs
@@ -6,11 +6,16 @@ using System.Threading.Tasks;
 
 namespace PolymorphismAssignment
 {
-    public class Employee : Person
+    public class Employee : Person, IQuittable
     {
         public override void SayName()
         {
             Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+
+        public void Quit()
+        {
+            throw new NotImplementedException();
         }
     }
 }
