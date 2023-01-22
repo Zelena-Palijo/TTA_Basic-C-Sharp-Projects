f2f313deff2723dc40f97d58530cb54516af38fb
Zelena Palijo <zpali853@gmail.com>
2023-01-22 10:50:58 -0800

    Clean up code and comments from Program

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -15,32 +15,16 @@ namespace TwentyOne
             Deck deck = new Deck(); //instatiated an object named deck object of Deck
             deck.Shuffle();
 
-
-            //ex, deck.Shuffle(3) >> calls method on itself
-            //int timesShuffled = 0;
-            //deck = Shuffle(deck, out timesShuffled, 3);
-            //^Changed so that it appears cleaner, calls edited method Shuffle()
-            
             
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine(deck.Cards.Count);
-            //Console.WriteLine("Times shuffled {0}", timesShuffled);
+           
             Console.ReadLine();
 
         }
 
-       
-
-        //public static Deck Shuffle(Deck deck, int times)
-        //{
-        //    for (int i = 0; i < times; i++)
-        //    {
-        //        deck = Shuffle(deck);
-        //    }
-        //    return deck;
-        //}
     }
 }
