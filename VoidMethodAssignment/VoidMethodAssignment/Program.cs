234566b322b7d5e46dd7dc13d7ebcdd4b09919a7
Zelena Palijo <zpali853@gmail.com>
2023-01-21 19:28:16 -0800

    comments

--- VoidMethodAssignment/VoidMethodAssignment/Program.cs
+++ VoidMethodAssignment/VoidMethodAssignment/Program.cs
@@ -10,6 +10,17 @@ namespace VoidMethodAssignment
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Pick a number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Pick a second number.");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = Math.Sum(num1, num2); // call the method, but void 
+            Console.WriteLine(num1 + " plus " + num2 + " equals " + result);
+            Console.ReadLine();
+
+            //cannot run due to void method?
         }
     }
 }
