4be24ebb2ee655e14b7dff5efc27bfae4dba1ba2
Zelena Palijo <zpali853@gmail.com>
2023-01-22 14:17:28 -0800

    crated abstract class, method, called method

---
+++ AbstractClassAssignment/AbstractClassAssignment/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public abstract void SayName(); //abstract method
+        
+    }
+}
---
+++ AbstractClassAssignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
