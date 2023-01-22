f22dd7619e3f0520c13a05c54ddef66aaa242470
Zelena Palijo <zpali853@gmail.com>
2023-01-21 18:47:27 -0800

    troubleshoot optional

--- MethodAssignment/MethodAssignment/Program.cs
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -13,11 +13,30 @@ namespace MethodAssignment
             //calling first method
             Console.WriteLine("Pick a number.");
             int num1 = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("(Optional) Pick a second number.");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            int result1 = Math.MultiplyBy5(num1, num2);
-            Console.WriteLine(num1 + " multiplied by 5 and then by {0} equals " + result1, num2);
-            Console.ReadLine();
+            string checkNum2 = Console.ReadLine();
+
+            //shows error if no input, so use if/else to handle this
+            // if user inputs something then convert to int and proceed 
+            if (!string.IsNullOrEmpty(checkNum2))
+            {
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                int result1 = Math.MultiplyBy5(num1, num2);
+                Console.WriteLine(num1 + " multiplied by 5 and then by {0} equals " + result1, num2);
+                Console.ReadLine();
+            }
+            else
+            {
+                int num2 = 2;
+                int result1 = Math.MultiplyBy5(num1, num2);
+                Console.WriteLine(num1 + " multiplied by 5 and then by {0} equals " + result1, num2);
+                Console.ReadLine();
+            }
+
+            //int result1 = Math.MultiplyBy5(num1, num2);
+            //Console.WriteLine(num1 + " multiplied by 5 and then by {0} equals " + result1, num2);
+            //Console.ReadLine();
         }
     }
 }
