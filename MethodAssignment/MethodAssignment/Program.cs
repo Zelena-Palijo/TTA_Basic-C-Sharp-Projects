8a5b6be89df04916db52084bb683372c028a1797
Zelena Palijo <zpali853@gmail.com>
2023-01-21 18:30:27 -0800

    call first method, ask for 2 integers, at least 1

--- MethodAssignment/MethodAssignment/Program.cs
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -10,6 +10,14 @@ namespace MethodAssignment
     {
         static void Main(string[] args)
         {
+            //calling first method
+            Console.WriteLine("Pick a number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("(Optional) Pick a second number.");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int result1 = Math.MultiplyBy5(num1, num2);
+            Console.WriteLine(num1 + " multiplied by 5 and then by {0} equals " + result1, num2);
+            Console.ReadLine();
         }
     }
 }
