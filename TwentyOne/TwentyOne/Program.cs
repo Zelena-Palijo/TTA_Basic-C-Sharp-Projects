f31e8150abce5f92d0c7970fa8d9bb4cad1ef9fd
Zelena Palijo <zpali853@gmail.com>
2023-01-22 16:49:00 -0800

    update

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,9 +10,15 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-
+            Game game = new TwentyOneGame();
+            game.Players = new List<Player>();
+            Player player = new Player();
+            player.Name = "Jesse";
+            game = game + player;
+            game = game - player;
+            
             Deck deck = new Deck(); //instatiated an object named deck object of Deck
-            deck.Shuffle();
+            deck.Shuffle(3);
 
             foreach (Card card in deck.Cards)
             {
