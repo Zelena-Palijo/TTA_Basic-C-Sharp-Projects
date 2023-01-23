2af604b0c90ba6f0356ad9d74332bb77f7d7771b
Zelena Palijo <zpali853@gmail.com>
2023-01-23 13:28:01 -0800

    fixed method 1 to call method without converting in main

--- MainMethodAssignment/MainMethodAssignment/Program.cs
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -12,26 +12,27 @@ namespace MainMethodAssignment
         {
             // calling first method int > int
             Console.WriteLine("Please choose a number.");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            int result1 = Math.MultiplyBy5(num1);
-            Console.WriteLine(num1 + " multiplied by 5 is " + result1);
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            int result1 = Math.MultiplyBy5(Console.ReadLine());
+            Console.WriteLine("The answer is "+ result1);
+            //Console.WriteLine(num1 + " multiplied by 5 is " + result1);
             Console.ReadLine();
 
-            //calling second method decimal >int
-            Console.WriteLine("Please choose a number with a decimal.");
-            decimal num2 = System.Convert.ToDecimal(Console.ReadLine()); // convert string to decimal
-            int result2 = Math.DivideBy2(num2); // method will convert decimal values to int 
+            ////calling second method decimal >int
+            //Console.WriteLine("Please choose a number with a decimal.");
+            //decimal num2 = System.Convert.ToDecimal(Console.ReadLine()); // convert string to decimal
+            //int result2 = Math.DivideBy2(num2); // method will convert decimal values to int 
 
-            Console.WriteLine(num2 + " divided by 2 is " + result2);
-            Console.ReadLine();
+            //Console.WriteLine(num2 + " divided by 2 is " + result2);
+            //Console.ReadLine();
 
-            //calling third method string > int
-            Console.WriteLine("Please choose a third number.");
-            string num3 = Console.ReadLine(); // no need to convert since method will do it
-            int result3 = Math.Add50(num3);
+            ////calling third method string > int
+            //Console.WriteLine("Please choose a third number.");
+            //string num3 = Console.ReadLine(); // no need to convert since method will do it
+            //int result3 = Math.Add50(num3);
 
-            Console.WriteLine(num3 + " added by 50 is " + result3);
-            Console.ReadLine();
+            //Console.WriteLine(num3 + " added by 50 is " + result3);
+            //Console.ReadLine();
         }
     }
 }
