d529c68e1cace6b3f3ef7028329d319ddc0cfd80
Zelena Palijo <zpali853@gmail.com>
2023-01-23 16:15:11 -0800

    Made so only 1 arg if 1 input

--- MethodAssignment/MethodAssignment/Program.cs
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -26,16 +26,16 @@ namespace MethodAssignment
                 int num2 = Convert.ToInt32(checkNum2);
                 //when above is processed, throws error that input method doesn't work
                 //worked the first time, but when input for second is 2, then error
-                int result1 = Math.MultiplyBy5(num1, num2);
-                Console.WriteLine(num1 + " multiplied by 5 and then by {0} equals " + result1, num2);
-                //Console.ReadLine();
+                int result2 = math.MultiplyBy5(num1, num2);
+                Console.WriteLine(result2);
+                Console.ReadLine();
             }
             else
             {
-                int num2 = 2; // have to declare here, but method should already do this?s
-                int result1 = Math.MultiplyBy5(num1, num2);
-                Console.WriteLine(num1 + " multiplied by 5 and then by {0} equals " + result1, num2);
-                //Console.ReadLine();
+                //int num2 = 2; // have to declare here, but method should already do this?s
+                int result = math.MultiplyBy5(num1);
+                Console.WriteLine(result);
+                Console.ReadLine();
             }
 
             //int result1 = Math.MultiplyBy5(num1, num2);
