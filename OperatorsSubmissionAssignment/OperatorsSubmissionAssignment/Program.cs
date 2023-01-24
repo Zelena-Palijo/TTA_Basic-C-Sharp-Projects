b2e7fdad3d1cf01a8c0467808d9e0494a3b0fe9c
Zelena Palijo <zpali853@gmail.com>
2023-01-23 19:21:08 -0800

    created operators submission assignment, class employee

---
+++ OperatorsSubmissionAssignment/OperatorsSubmissionAssignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
