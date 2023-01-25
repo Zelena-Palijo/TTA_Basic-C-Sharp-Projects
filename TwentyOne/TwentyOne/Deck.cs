71007ea940a873c0640a9d3967aa16ea020b6f2f
Zelena Palijo <zpali853@gmail.com>
2023-01-24 21:58:07 -0800

    Cleanup

--- TwentyOne/TwentyOne/Deck.cs
+++ TwentyOne/TwentyOne/Deck.cs
@@ -11,32 +11,9 @@ namespace TwentyOne
         //add constructor here
         public Deck()
         {
-            //Cards = new List<Card>();
-            //Card cardOne = new Card();
-            //cardOne.Face = "Two";
-            //cardOne.Suit = "Hearts";
-            //Cards.Add(cardOne);
-
             //easier way using nested for/each loop instead of writing it out
             Cards = new List<Card>();
-            //list of all possible suits
-            List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
-            List<string> Faces = new List<string>()
-            {
-                "Two", "Three", "Four", "Five", "Six", "Seven",
-                "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"
-            };
-
-            foreach (string face in Faces)
-            {
-                foreach(string suit in Suits)
-                {
-                    Card card = new Card();
-                    card.Suit = suit;
-                    card.Face = face;
-                    Cards.Add(card);
-                }
-            }
+            
 
         }
 
