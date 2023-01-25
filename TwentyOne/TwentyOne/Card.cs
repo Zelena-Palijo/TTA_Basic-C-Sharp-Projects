361bc8acb28620b6d54f078ee08cbbf96b5721d9
Zelena Palijo <zpali853@gmail.com>
2023-01-24 21:51:09 -0800

    update

--- TwentyOne/TwentyOne/Card.cs
+++ TwentyOne/TwentyOne/Card.cs
@@ -9,12 +9,7 @@ namespace TwentyOne
     public class Card
     {
         // constructor method name is always the name of the class
-        public Card()
-        {
-            Suit = "Spades";
-            Face = "Two";
-
-        }
+        
         //Giving Card class a property 
         public Suit Suit { get; set; }
         public string Face { get; set; }
