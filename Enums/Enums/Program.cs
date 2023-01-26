d2517f0a39639c1feb88bcb4b0a2acdcd894c99a
Zelena Palijo <zpali853@gmail.com>
2023-01-25 17:53:54 -0800

    try/catch block for user prompt

--- Enums/Enums/Program.cs
+++ Enums/Enums/Program.cs
@@ -10,18 +10,47 @@ namespace Enums
     {
         static void Main(string[] args)
         {
+            try
+            {
+                //ask user to input current day of the week
+                Console.WriteLine("What day of the week is it?");
+                string value = Console.ReadLine();
+
+                //Convert string to enum
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), value);
+
+                //see if the conversion worked
+                //does it have to ask system what current day of the week it actually is?
+                if (day == DaysOfTheWeek.Thursday) 
+                {
+                    Console.WriteLine("is the same day");
+                    //Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                return;
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+
+            
             
         }
 
         public enum DaysOfTheWeek
         {
-            Sunday,
-            Monday,
-            Tuesday,
-            Wednesday,
-            Thursday,
-            Friday,
-            Saturday
+            Monday = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6,
+            Sunday = 7
         }
     }
 }
