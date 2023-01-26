efd08ec3944ca2f8585ccc7cbfb29f569e21c000
Zelena Palijo <zpali853@gmail.com>
2023-01-25 17:43:20 -0800

    Created enum project, enum daysoftheweek

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public enum DaysOfTheWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}
