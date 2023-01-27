f3580618eb06158b4cde01d2acccacd2b0cf0b84
Zelena Palijo <zpali853@gmail.com>
2023-01-27 14:07:16 -0800

    Added start of game to main method

--- TwentyOne/TwentyOne/Player.cs
+++ TwentyOne/TwentyOne/Player.cs
@@ -8,18 +8,26 @@ namespace TwentyOne
 {
     public class Player
     {
+        //Constructor
+        public Player(string name, int beginningBalance)
+        {
+            Hand = new List<Card>();
+            Balance = beginningBalance;
+            Name = name;
+        }
+
         public List<Card> Hand { get; set; }
         public int Balance { get; set; }
         public string Name { get; set; }
         public bool isActivelyPlaying { get; set; }
 
-        public static Game operator+ (Game game, Player player)
+        public static Game operator +(Game game, Player player)
         {
             game.Players.Add(player);
             return game;
         }
 
-        public static Game operator- (Game game, Player player)
+        public static Game operator -(Game game, Player player)
         {
             game.Players.Remove(player);
             return game;
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,7 +10,23 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Welcome to the Grand Hotel and Casino! First, what is your name?");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("Great! How much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank); //create new player
+                Game game = new TwentyOneGame(); //polymorphism
+                game += player;
+                player.isActivelyPlaying = true; //while loop later to keep game going if playing
+                while(player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+            }
         }
 
     }
