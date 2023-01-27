f6d2007307f37e76dacae624db51b8661bb061c9
Zelena Palijo <zpali853@gmail.com>
2023-01-27 13:49:34 -0800

    clean up main method to start

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,22 +10,7 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Deck deck = new Deck(); //instatiated an object named deck object of Deck
-
-            //int count = deck.Cards.Count(x => x.Face == Face.Ace);
-            //lambda function
-
-
-
-            //deck.Shuffle(3);
-
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
-            Console.ReadLine();
-
+            
         }
 
     }
