75d8218f328dc514c74ec5ae3b614eaeee15f1b3
Zelena Palijo <zpali853@gmail.com>
2023-01-27 21:11:51 -0800

    created getallpossiblevalues, checkforblackjack methods

--- TwentyOne/TwentyOne/TwentyOneRules.cs
+++ TwentyOne/TwentyOne/TwentyOneRules.cs
@@ -24,5 +24,28 @@ namespace TwentyOne
             [Face.King] = 10,
             [Face.Ace] = 1 //can be worth 1 or 11 depending on player
         };
+
+        private static int[] GetAllPossibleHandValues(List<Card> Hand)
+        {
+            int aceCount = Hand.Count(x => x.Face == Face.Ace);
+            int[] result = new int[aceCount + 1];
+            int value = Hand.Sum(x => _cardValues[x.Face]); //checks for lowest/first value, where Ace = 1
+            result[0] = value; 
+            if (result.Length == 1) return result;
+            for (int i = 1; i < result.Length; i++) //start i=1 because 0 above is lowest value
+            {
+                value += (i * 10); // next value is value + 1, times 10 
+                result[i] = value;
+            } //assigned all possible integer values to integer array "result"
+            return result;
+        }
+
+        public static bool CheckForBlackJack(List<Card> Hand)
+        {
+            int[] possibleValues = GetAllPossibleHandValues(Hand);
+            int value = possibleValues.Max(); //getting maximum possible value
+            if (value == 21) return true; // yes blackjack
+            else return false;
+        }
     }
 }
