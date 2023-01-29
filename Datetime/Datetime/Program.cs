c0e4dcdc629462629d514ba666742a4c37a36fe8
Zelena Palijo <zpali853@gmail.com>
2023-01-28 21:21:32 -0800

    add datetime

---
+++ Datetime/Datetime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime rightNow = DateTime.Now; //set value for current time
+            Console.WriteLine(rightNow); //print current date and time to console
+            
+            Console.WriteLine("Pick a number."); 
+            //later time set, use addhours datetime method
+            DateTime laterTime = rightNow.AddHours(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("It will be " + laterTime + " soon enough...");
+            Console.ReadLine();
+
+        }
+    }
+}
