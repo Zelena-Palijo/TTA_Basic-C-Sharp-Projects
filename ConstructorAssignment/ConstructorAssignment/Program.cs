7e319c47a9b2e93c9ff2ad6abb29f9a9dcaa7beb
Zelena Palijo <zpali853@gmail.com>
2023-01-29 20:55:43 -0800

    add const

--- ConstructorAssignment/ConstructorAssignment/Program.cs
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -10,9 +10,17 @@ namespace ConstructorAssignment
     {
         static void Main(string[] args)
         {
+            const string schoolMotto = "Learn or else";
+            Console.WriteLine(schoolMotto);
+
             Student newStudent = new Student("Zelena");
 
             Console.WriteLine(newStudent.Id); 
+            //Console.ReadLine(); //should automatically output 11111
+
+            var otherStudent = new Student("Christopher");
+            //automatically detect type
+            Console.WriteLine(otherStudent.Id);
             Console.ReadLine(); //should automatically output 11111
 
         }
