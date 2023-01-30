73177f7c9869dcc7f42fddf2ab69240537122158
Zelena Palijo <zpali853@gmail.com>
2023-01-30 14:20:43 -0800

    add try catch

---
+++ TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("How old are you?");
+                int age1 = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                int age1 = Convert.ToInt32(Console.ReadLine());
+                if (age1 <= 0) Console.WriteLine("Please enter a positive number.");
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error has occurred. Please try again.");
+                Console.ReadLine();
+                return;
+            }
+
+
+
+        }
+    }
+}
