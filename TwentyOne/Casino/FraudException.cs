c680c223f88597e784a3341b86e476852b8b5d50
Zelena Palijo <zpali853@gmail.com>
2023-01-30 13:55:08 -0800

    create fraud exception class

--- TwentyOne/Casino/FraudException.cs
+++ TwentyOne/Casino/FraudException.cs
@@ -6,5 +6,11 @@ namespace Casino
 {
     public class FraudException : Exception
     {
+        public FraudException()
+            : base() { } //inherit from base exception
+
+        public FraudException(string message)
+            : base(message) { }
+        
     }
 }
