dcbfccc4302d16870840d924ea0e1385f3ef2521
Zelena Palijo <zpali853@gmail.com>
2023-02-06 11:29:07 -0800

    create list of newsletter signups from db

--- NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NewsletterAppMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -10,6 +11,8 @@ namespace NewsletterAppMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
         public ActionResult Index()
         {
             return View();
@@ -23,8 +26,7 @@ namespace NewsletterAppMVC.Controllers
             }
             else
             {
-                string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-
+                
                 string queryString = @"INSERT INTO SignUps(FirstName, LastName, EmailAddress) VALUES
                                         (@FirstName, @LastName, @EmailAddress)";
                 using (SqlConnection connection = new SqlConnection(connectionString))
@@ -48,7 +50,30 @@ namespace NewsletterAppMVC.Controllers
         
         public ActionResult Admin()
         {
+            string queryString = @"SELECT Id, FirstName, LastName, EmailAddress from SignUps";
+            List<NewsletterSignUp> signups = new List<NewsletterSignUp>();
+
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var signup = new NewsletterSignUp();
+                    signup.Id = Convert.ToInt32(reader["Id"]);
+                    signup.FirstName = reader["FirstName"].ToString();
+                    signup.LastName = reader["LastName"].ToString();
+                    signup.EmailAddress = reader["EmailAddress"].ToString();
+                    signups.Add(signup);
+                }
+            }
+
             return View();
+            
         }
     }
 }
