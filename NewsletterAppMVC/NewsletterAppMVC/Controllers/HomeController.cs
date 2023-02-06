e8b2ab05556459cd7facd1981f0410c2eee895ce
Zelena Palijo <zpali853@gmail.com>
2023-02-06 11:16:17 -0800

    add to admin model page

--- NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
@@ -45,17 +45,9 @@ namespace NewsletterAppMVC.Controllers
                 return View("Success");
             }
         }
-        public ActionResult About()
+        
+        public ActionResult Admin()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
