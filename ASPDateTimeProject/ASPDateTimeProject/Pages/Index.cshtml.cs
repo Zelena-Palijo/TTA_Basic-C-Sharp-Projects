61890b87fb97fa1fb475fea4e87ed0ca503f5bdf
Zelena Palijo <zpali853@gmail.com>
2023-02-15 11:05:49 -0800

    Create ASP web app to publish to Azure

---
+++ ASPDateTimeProject/ASPDateTimeProject/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+
+namespace ASPDateTimeProject.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToString("r", new CultureInfo("en-US"));
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
+}
