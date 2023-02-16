a70bbebc7b8ac3a30f06c9f2ae6d24bf59e461ce
Zelena Palijo <zpali853@gmail.com>
2023-02-15 20:02:52 -0800

    Create student and grade class

---
+++ EFStudentdb1/EFStudentdb1/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFStudentdb1
+{
+    internal class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        public Grade Grade { get; set; }
+    }
+}
